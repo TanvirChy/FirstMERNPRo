8393140157c98eb69b48e53ad2c894944b4bc61e
TanvirChy <tanvirchy4242@gmail.com>
2021-06-25 20:49:25 +0600

    first commit

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import './header.css'
+
+export default function Header() {
+    return (
+        <div className='header'>
+            <div className='headerTitles'>
+                <span className='headerTitleSm'>React & Node</span>
+                <span className='headerTitleLg' >Blog</span>
+            </div>
+            <img src='https://cdn.dnaindia.com/sites/default/files/styles/full/public/2021/01/14/949927-srk.jpg' alt='' />
+        </div>
+    )
+}
